6a946079f5f50222abd40263556c9f3df2d80334
Ajeet Mishra <ajeetmishra@gmail.com>
2019-09-25 16:56:27 +0530

    Started student name guessing

--- fees_processing.py
+++ fees_processing.py
@@ -1,7 +1,8 @@
 import os, xlrd, re
+import numpy as np
 import pandas as pd
 
-os.chdir('/home/qf/fees_rgkkh')
+os.chdir('/home/qf/Documents/QfProjects/fees_rgkkh/')
 
 class fee_file:
     
@@ -43,14 +44,95 @@ class fee_file:
         self.count_rows = self.last_row - 21
 
         self.data = self.get_data()
-        print(self.data.head())
+        print(self.data.tail(60))
+
+        # self.data = self.process_narrations()
 
 
     def get_data(self):
         df = pd.read_excel(self.fname, skiprows=21, nrows=self.count_rows)
         df.columns = ["Dt", "Narr", "Ref", "Dt_post", "Dr", "Cr", "Close",]
+        df.Dt = pd.to_datetime(df.Dt)
         
-    
+        #Remove debit entries
+        df = df[np.isnan(df.Dr)]
+
+        #Remove credit entries of Internal Transfer
+        df = df[df.Narr.str.contains('INTERNAL TRANSFER') == False]
+
+        #Remove NEFT returns
+        df = df[df.Narr.str.contains('NEFT RETURN') == False]
+
+        #Drop columns not needed
+        df = df.drop(columns=['Dt_post', 'Dr', 'Close'])
+        df['Mode'] = df.Narr.apply(self.process_narrations)
+
+        #Guess student name
+        df['Student'] = df.Narr.apply(self.guess_student)
+        
+        #Sort by student name
+        df = df.sort_values('Student')
+
+        return df
+
+
+
+    def process_narrations(self, text):
+        if text.startswith('IMPS'):
+            return 'IMPS'
+        elif text.startswith('NEFT'):
+            return 'NEFT'
+        elif '-TPT-' in text:
+            return 'TPT'
+        elif text.startswith('UPI'):
+            return 'UPI'
+        elif 'NET BANKING SI' in text:
+            return 'SI'
+        elif 'CHQ DEP' in text:
+            return 'CHQ'
+        elif 'CASH DEP' in text:
+            return 'CASH'
+        else:
+            return ''
+
+        
+    def guess_student(self, text):
+        pattern_m = {}
+        pattern_m['XX - Sumit/Vidula Naikode'] = 'SHAILESH DAGADU'
+        pattern_m['XX - Kali/Shri Shukla'] = 'KALI SHRI'
+        pattern_m['PP - Aarohi Mishra'] = 'AAROHI ISHI'
+        pattern_m['03 - Dev Madkaikar'] = 'MAST DEV'
+        pattern_m['03 - Tashvi Vartak'] = 'TASHVI'
+        pattern_m['04 - Joy Tanna'] = 'JOY TANNA'
+        pattern_m['04 - Agrima Mishra'] = 'AGRIMA'
+        pattern_m['05 - Gaurav Chopra'] = 'RAHUL VINOD CHOPRA'
+        pattern_m['05 - Rohit Nayak'] = 'RASHMI V NAYAK'
+        pattern_m['06 - Isha Bapat'] = 'ISHA BAPAT'
+        pattern_m['07 - Yash Gajra'] = 'YASH GAJRA'
+        pattern_m['07 - Harsh Ambesange'] = 'RAKHEE SUDHANSHU AMBESANGE'
+        pattern_m['07 - Jaivik Vyas'] = 'RENUKADARSHANVYAS'
+        pattern_m['07 - Saee Gujare'] = 'PRASHANT DATTU GUJAR'
+        pattern_m['08 - Aditya Mahajan'] = 'ANIL N MAHAJAN'
+        pattern_m['08 - Diya Pamnani'] = 'PALLAVI HIRANAND PAMN'
+        pattern_m['08 - Rishi Agarwal'] = 'MINU AGARWAL'
+        pattern_m['08 - Yuvaraj Mudaliyar'] = 'VIJAY RAMSWAMY MUDALIYAR'
+        pattern_m['08 - Yuvaraj Mudaliyar'] = 'VIJAY RAMSWAMY MUDAL'
+        pattern_m['09 - Aditya Bhise'] = 'NITIN CHHAGAN BHISE'
+        pattern_m['09 - Moulya'] = 'MOULYA'
+        pattern_m['09 - Krushna Borhade'] = 'BORHADE'
+        pattern_m['10 - Shubh Shah'] = 'NAYAN VASANT SHAH'
+        pattern_m['10 - Rucha Shelar'] = 'SANJAY S SHELAR'
+        pattern_m['10 - Ved Ahire'] = 'VED AHIRE'
+        pattern_m['10 - Rasika Desai'] = 'CHAITRA DESAI'
+        pattern_m['10 - Bhavya'] = 'BHAVYA PATEL'
+        pattern_m['10 - Arya Gawas'] = 'PRADIP GOVIND GAWAS'
+        pattern_m['07 - Sarthak Sawant'] = 'SARTHAK FEE'
+
+        for key, val in pattern_m.items():
+            if val in text:
+                return key
+        return None
+
     def __str__(self):
         ret = '\n'.join([self.fname, self.ac_no, self.ac_type, self.date_from + ' - ' + self.date_to \
             , str(self.last_row), str(self.count_rows) \
